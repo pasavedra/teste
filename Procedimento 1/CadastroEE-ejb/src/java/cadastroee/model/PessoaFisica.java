355d4966c79318ceb5e3b463011013ff575024e0
Paola Savedra Barreiros <145854710+pasavedra@users.noreply.github.com>
2024-11-05 11:54:38 -0300

    Add files via upload

---
+++ Procedimento 1/CadastroEE-ejb/src/java/cadastroee/model/PessoaFisica.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import java.io.Serializable;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author pasav
+ */
+@Entity
+@Table(name = "pessoa_fisica")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "PessoaFisica.findAll", query = "SELECT p FROM PessoaFisica p"),
+    @NamedQuery(name = "PessoaFisica.findByIdpessoa", query = "SELECT p FROM PessoaFisica p WHERE p.idpessoa = :idpessoa"),
+    @NamedQuery(name = "PessoaFisica.findByCpf", query = "SELECT p FROM PessoaFisica p WHERE p.cpf = :cpf")})
+public class PessoaFisica implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idpessoa")
+    private Integer idpessoa;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "cpf")
+    private String cpf;
+    @JoinColumn(name = "idpessoa", referencedColumnName = "idpessoa", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Pessoa pessoa;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(Integer idpessoa) {
+        this.idpessoa = idpessoa;
+    }
+
+    public PessoaFisica(Integer idpessoa, String cpf) {
+        this.idpessoa = idpessoa;
+        this.cpf = cpf;
+    }
+
+    public Integer getIdpessoa() {
+        return idpessoa;
+    }
+
+    public void setIdpessoa(Integer idpessoa) {
+        this.idpessoa = idpessoa;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idpessoa != null ? idpessoa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PessoaFisica)) {
+            return false;
+        }
+        PessoaFisica other = (PessoaFisica) object;
+        if ((this.idpessoa == null && other.idpessoa != null) || (this.idpessoa != null && !this.idpessoa.equals(other.idpessoa))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.PessoaFisica[ idpessoa=" + idpessoa + " ]";
+    }
+    
+}
